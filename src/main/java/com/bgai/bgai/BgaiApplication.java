b3a80fc23d96a6d1ccc7e94d7fe067e0f60789e4
asksamalam <asksamalam@gmail.com>
2025-02-09 17:10:30 +0530

    Initial Commit

---
+++ src/main/java/com/bgai/bgai/BgaiApplication.java
@@ -0,0 +1,13 @@
+package com.bgai.bgai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bgai/bgai/BgaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgai.bgai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
